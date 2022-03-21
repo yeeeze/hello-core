0e63053ae98b819f4f54a5773c339a98048cc6c1
yeezze <86194303+yeezze@users.noreply.github.com>
2022-03-21 16:05:19 +0900

    필터
    ComponentScan에 포함시키거나 제외시키거나

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -3,6 +3,7 @@ package hello.core;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(
