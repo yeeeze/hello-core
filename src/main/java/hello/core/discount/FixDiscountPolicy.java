6bd8319ac4ceed87a95eedfc1a5107da31419fb5
yeezze <86194303+yeezze@users.noreply.github.com>
2022-04-04 19:38:32 +0900

    조회 빈이 2개 이상 문제 - @Primary

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,10 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 // 고정 할인정책
 public class FixDiscountPolicy implements DiscountPolicy{
 
